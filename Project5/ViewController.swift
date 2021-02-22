5be557eb352bf8a8f2100a77d02c81cb30eec94c
FrankSolleveld <fsolleveld@outlook.com>
2021-02-22 10:34:21 +0100

    added words + base code for tableview and StartGame() method

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    // MARK: Custom Variables
+    var allWords = [String]()
+    var usedWords = [String]()
 
+    // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["bench"]
+        }
+        startGame()
+    }
+    
+    // MARK: Custom Methods
+    func startGame(){
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 
-
+    // MARK: Delegate Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
