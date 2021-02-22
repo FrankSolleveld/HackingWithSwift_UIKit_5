1a9ad8db8cc63de9bd63ab2e6a773b7a3dfc7204
FrankSolleveld <fsolleveld@outlook.com>
2021-02-22 10:56:36 +0100

    created submit functionality

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -49,7 +49,28 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
-        
+        let lowerAnswer = answer.lowercased()
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isOriginal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
     }
 
     // MARK: Delegate Methods
